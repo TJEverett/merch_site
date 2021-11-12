40af41d93eafc34376c3682a9706f5e8cb22c02c
Tristen Everett <spock214@gmail.com>
2021-11-12 13:53:19 -0800

    restructure to begin work on project

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <React.Fragment>
+      <h1>Header Here</h1>
+      <h1>Body Here</h1>
+    </React.Fragment>
+  );
+}
+
+export default App;
